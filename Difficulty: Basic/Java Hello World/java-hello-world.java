4652ce37bd4cc7212cc8eb608b5fea8d0275fbfc
Bhulokam Haritha <162732025+softwarewalahari@users.noreply.github.com>
2025-07-07 22:11:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Hello World/java-hello-world.java
@@ -0,0 +1,11 @@
+class Solution {
+    
+    static void printHelloWorld() {
+        String message = "Hello World";
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        printHelloWorld();  // Call the method
+    }
+}
