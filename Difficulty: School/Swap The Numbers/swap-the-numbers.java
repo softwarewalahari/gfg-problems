0125c99e8e7e20279d5abc26eafa6ce5a48ea441
Bhulokam Haritha <162732025+softwarewalahari@users.noreply.github.com>
2025-07-08 22:27:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+       // int temp=a;
+    //    a=b;
+      //  b=temp;
+      a=a+b;
+      b=a-b;
+      a=a-b;
+      
+
+        System.out.println(a + " " + b);
+    }
+}
