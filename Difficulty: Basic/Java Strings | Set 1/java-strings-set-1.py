6835db27ce6f6e0ffc71f2d1d783698e0e842439
Bhulokam Haritha <162732025+softwarewalahari@users.noreply.github.com>
2025-07-25 22:09:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Strings | Set 1/java-strings-set-1.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+class Solution:
+    def conRevstr (ob, S1, S2):
+        # code here 
+        combined=S1+S2
+        result=combined[::-1]
+        return result
