45281e3661b830633cb1db76465d9278baff249e
Bhulokam Haritha <162732025+softwarewalahari@users.noreply.github.com>
2025-07-25 23:21:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int nSum(int n) {
+        // code here
+        int sum=0;
+     for(int i=1;i<=n;i++)
+     {
+         sum=sum+i;
+     }
+        return sum;
+    }
+}
