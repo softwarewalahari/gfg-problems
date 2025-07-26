292b62d955eb4911cb59f334f6150660fbb8ade4
Bhulokam Haritha <162732025+softwarewalahari@users.noreply.github.com>
2025-07-26 19:32:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else Statement/the-else-statement.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+class Solution {
+    public static void utility(int number) {
+        // Write your if-else statement below
+      String result;  
+  if (number> 100)
+  {
+      result= "Big";
+  }
+  else
+  {
+      result= "Number ";
+  }
+      System.out.println(result);
+       
+    }
+   
+}
