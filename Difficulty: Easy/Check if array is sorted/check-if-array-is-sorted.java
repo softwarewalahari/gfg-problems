465ff956316dcc1703fb1d84f787a66f3e7ad521
Bhulokam Haritha <162732025+softwarewalahari@users.noreply.github.com>
2025-08-13 19:33:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.java
+++ Difficulty: Easy/Check if array is sorted/check-if-array-is-sorted.java
@@ -3,14 +3,17 @@ class Solution
     public boolean isSorted(int[] arr) 
     {
         // code here
+         boolean sorted=true;
         for(int i=0;i<arr.length-1;i++)
         {
+           
                if(arr[i] > arr[i+1])
                {
-                  return false;
+                  sorted=false;
+                  break;
                }
         }
-           return true;
+           return sorted;
         
     }
 }
