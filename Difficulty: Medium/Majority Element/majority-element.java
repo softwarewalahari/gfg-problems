565d2baceebac6bb6e49198b8161a94e412b393a
Bhulokam Haritha <162732025+softwarewalahari@users.noreply.github.com>
2025-08-21 19:01:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -1,47 +1,33 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().majorityElement(arr));
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    static int majorityElement(int arr[]) {
-        int count;
+    int majorityElement(int arr[]) {
+        // code here
+        int n=arr.length;
+        HashMap<Integer,Integer> res= new HashMap<>();
+        for(int i=0;i<n;i++)
+       {
+           res.put(arr[i],res.getOrDefault(arr[i],0) +1);
+           if(res.get(arr[i]) > n / 2)
+           {
+               return arr[i];
+               
+           }
+       }
+       return -1;
+        //    int count = 0;
+         //   for(int j=0;j<n;j++)
+           // {
+             //   if(arr[i] == arr[j])
+               // {
+                 //   count++;
+                //}
+        //    }
+          //  if(count > (n/2))
+            //{
+              //  return arr[i];
+            //}
+    //    }
+      //  return -1;
       
-        for( int i=0;i<arr.length;i++)
-        {
-            count=1;
-            for(int j=i+1;j<arr.length;j++)
-            {
-            if(arr[i] == arr[j])
-            {
-                count++;
-            }
-        }
-        if(count > arr.length/2 )
-        {
-            return arr[i];
-        }
-        }
-    
-    return -1;
     }
+    
 }
