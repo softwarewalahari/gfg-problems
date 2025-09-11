6992c2d8a11b578f231f70facd345bb5bcb4092b
Bhulokam Haritha <162732025+softwarewalahari@users.noreply.github.com>
2025-09-11 18:33:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 6/pattern-6.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i=n;i>0;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
